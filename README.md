485ea6533148e2ac93791b78ed3577e677437504
Tomasz Baron <tomek91b@gmail.com>
2018-07-15 11:28:50 +0200

    Add gulp tasks

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-To run build task enter: 
+Instal NPM packages
+```
 npm install
-node_modules/.bin/webpack
+```
 
-Build on Node.js v8.11.3
+Start project
+```
+gulp
+```
+Server starts on port 8000
+
+Type 
+```
+http://localhost:8000
+```
+in your browser.
+
+JSDocs is available in docs directory.
+
+Node.js v8.11.3
 
 Tested on Chromium (Ubuntu 18.04 LTS)
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,39 @@
 const gulp = require('gulp'),
-        jsdoc = require('gulp-jsdoc3');
+        connect = require('gulp-connect'),
+        jsdoc = require('gulp-jsdoc3'),
+        jshint = require('gulp-jshint'),
+        webpack = require('webpack'),
+        runSequence = require('gulp-run-sequence'),
+        config = require('./webpack.config.js');
+
  
 gulp.task('docs', function (cb) {
-    gulp.src(['README.md', './src/**/*.js'], {read: false})
-        .pipe(jsdoc(cb));
+        gulp.src(['README.md', './src/**/*.js'], {read: false})
+                .pipe(jsdoc(cb));
 });
+
+gulp.task('lint', function() {
+        return gulp.src('./src/*.js')
+                .pipe(jshint())
+                .pipe(jshint.reporter('default'));
+});
+
+gulp.task('build', function() {
+        webpack(config).run();
+        return gulp.src('index.html')
+                .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('server', function() {
+        connect.server({
+                port: 8000,
+                livereload: true,
+                root: ['dist']
+        });
+});
+
+gulp.task('serve', function() {
+        runSequence('docs', 'lint', 'build', 'server');
+});
+
+gulp.task('default', ['serve']);
